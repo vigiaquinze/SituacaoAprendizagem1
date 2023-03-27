0772060d7cd95144affd22562d724398e0642168
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-03-27 14:19:54 -0300

    27.3 14h19

--- Atividade2/App.java
+++ Atividade2/App.java
@@ -2,6 +2,7 @@ package Atividade2;
 
 public class App {
     public static void main(String[] args) {
-        
+        Atividade obj1 = new Atividade();
+        obj1.atividadeDois();
     }
 }
--- Atividade2/Atividade.java
+++ Atividade2/Atividade.java
@@ -1,7 +1,16 @@
 package Atividade2;
 
+import java.util.Scanner;
+
 public class Atividade {
+    Scanner scan = new Scanner(System.in);
     public void atividadeDois() {
-        
+        System.out.println("Digite o número da matrícula do aluno:");
+        int numeroMatricula = scan.nextInt();
+        int restoDivisao = numeroMatricula % 4;
+        if (restoDivisao==0){System.out.println("O aluno é do time Chris");}
+        else if (restoDivisao==1){System.out.println("O aluno é do time Greg");}
+        else if (restoDivisao==2){System.out.println("O aluno é do time Caruso");}
+        else if (restoDivisao==3){System.out.println("O aluno é do time Jerome");}
     }
 }
